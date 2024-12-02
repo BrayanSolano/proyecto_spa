740a85a2d95538bf1d32878ad8354d4f5584b1d8
BrayanSolano <brayansolanosanchez@gmail.com>
2024-12-02 12:05:24 -0500

    Creacion del componente heroe y configuración de su ruta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 
 
 //Servicios
-
+import { HeroesService } from "./services/heroes.service";
 
 
 //Componentes
@@ -15,6 +15,7 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
 
 @NgModule({
   declarations: [
@@ -22,13 +23,16 @@ import { HeroesComponent } from './components/heroes/heroes.component';
     NavbarComponent,
     HomeComponent,
     AboutComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    HeroesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HeroesService, Heroe } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
-export class HeroesComponent {
+export class HeroesComponent  implements OnInit {
+
+  heroes:Heroe[]=[]
+
+  // importamos el servicio 
+  constructor(  private _heroesService: HeroesService, private router:Router){
+    // console.log("constructor")
+  }
+
+  ngOnInit(): void {
+    this.heroes= this._heroesService.getHeroes();
+
+    // console.log(this.heroes)
+  }
+
+  verHeroe(idx: Number){
+    this.router.navigate(['/heroe',idx    ]);
+  }
 
 }
